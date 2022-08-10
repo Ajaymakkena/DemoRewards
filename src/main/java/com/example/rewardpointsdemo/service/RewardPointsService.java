5a103ee0cfbf0eac19057d7c6577ca2fc7815405
Ajaymakkena <ajaymakkena5090@gmail.com>
2022-08-10 05:55:17 -0400

    externalises amount one and amount two values

--- src/main/java/com/example/rewardpointsdemo/service/RewardPointsService.java
+++ src/main/java/com/example/rewardpointsdemo/service/RewardPointsService.java
@@ -1,21 +1,28 @@
 package com.example.rewardpointsdemo.service;
 
+import com.example.rewardpointsdemo.configuration.ApplicationProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class RewardPointsService {
 
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
     public int getRewardPoints(int amount) {
         int rewardPoints = 0;
+        int amountOne = applicationProperties.getAmounts().getAmountOne();
+        int amountTwo = applicationProperties.getAmounts().getAmountTwo();
 
-        if(amount > 50) {
-            rewardPoints += (amount - 50);
+        if(amount > amountOne) {
+            rewardPoints += (amount - amountOne);
         }
 
-        if(amount > 100) {
-            rewardPoints += (amount - 100);
+        if(amount > amountTwo) {
+            rewardPoints += (amount - amountTwo);
         }
 
         return rewardPoints;
