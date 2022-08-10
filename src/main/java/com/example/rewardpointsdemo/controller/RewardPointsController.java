9a7eab20dfa33acc7c41d2873230368ad7a6fb2e
Ajaymakkena <ajaymakkena5090@gmail.com>
2022-08-10 10:57:22 -0400

    adds unit test cases

--- src/main/java/com/example/rewardpointsdemo/controller/RewardPointsController.java
+++ src/main/java/com/example/rewardpointsdemo/controller/RewardPointsController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
